aef64ca934b7af39acb3e61d2c6766967a1cff79
lpuls <linepuls@gmail.com>
2017-07-06 17:33:13 +0800

    AC了ValidParentheses

---
+++ ValidParentheses/ValidParentheses.py
@@ -0,0 +1,25 @@
+﻿class ValidParentheses(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        brackets_dict = {'[': ']', '(': ')', '{': '}' }
+
+        length = len(s)
+        stack = list()
+        for item in s:
+            if '[' == item or '(' == item or '{' == item:
+                stack.append(item)
+            else:
+                stack_length = len(stack)
+                if stack_length <= 0:
+                    return False
+                top = stack[len(stack) - 1]
+                if brackets_dict[top] != item:
+                    return False
+                else:
+                    stack.pop()
+        return len(stack) == 0
+
+
