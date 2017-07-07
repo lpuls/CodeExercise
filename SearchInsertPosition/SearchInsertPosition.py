588fb9210a776fc4173f5a2f8aafaf034f0ba60c
lpuls <linepuls@gmail.com>
2017-07-07 15:43:50 +0800

    AC了SearchInsertPosition和ImplementstrStr 说起来ImplementstrStr是在逗逼吗……

---
+++ SearchInsertPosition/SearchInsertPosition.py
@@ -0,0 +1,23 @@
+﻿class SearchInsertPosition(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        length = len(nums)
+        index = 0
+        is_search = False
+        for i in range(0, length):
+            if target < nums[i] and not is_search:
+                index = i
+                is_search = True
+            elif target == nums[i]:
+                return i
+        if length <= 0 or (length > 0 and nums[0] > target):
+            return 0
+        elif length > 0 and nums[length - 1] < target:
+            return length 
+        else:
+            return index
+
