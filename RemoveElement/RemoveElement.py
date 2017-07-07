5a886ee6b4486080176a0c8e21000a6d09e12c5a
lpuls <linepuls@gmail.com>
2017-07-07 15:20:18 +0800

    AC了RemoveElement

---
+++ RemoveElement/RemoveElement.py
@@ -0,0 +1,18 @@
+﻿class RemoveElement(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        index = 0
+        while index < len(nums):
+            value = nums[index]
+            if value == val:
+                del nums[index]
+            else:
+                index += 1
+        return index;
+
+
+
