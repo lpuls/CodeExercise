517a072c20c641538c93acc0e8cc881aae0a7850
lpuls <linepuls@gmail.com>
2017-07-07 15:03:23 +0800

    AC了RemoveDuplicatesFromSortedArray

---
+++ RemoveDuplicatesFromSortedArray/RemoveDuplicatesFromSortedArray.py
@@ -0,0 +1,22 @@
+﻿class RemoveDuplicatesFromSortedArray(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if len(nums) <= 0:
+            return 0
+
+        count = 1
+        current = nums[0]
+        for item in nums:
+            if item != current:
+                current = item
+                nums[count] = current
+                count += 1
+
+        return count
+
+
+
+
