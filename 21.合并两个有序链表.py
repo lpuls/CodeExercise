6c9763d3d8947210b7ff01202d60b0d7dbc22116
xp <linepuls@gmail.com>
2019-03-25 19:44:13 +0800

    AC 21

--- 21.合并两个有序链表.py
+++ 21.合并两个有序链表.py
@@ -21,33 +21,25 @@
 # 
 #
 # Definition for singly-linked list.
-class ListNode:
-    def __init__(self, x):
-        self.val = x
-        self.next = None
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
 
 class Solution:
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
-        result = None
+        result = ListNode(0)
+        f = result
         l1_node = l1
         l2_node = l2
-        f = None
         while l1_node and l2_node:
             if l1_node.val < l2_node.val:
-                if not result:
-                    result = ListNode(l1_node.val)
-                    f = result
-                else:
-                    result.next = ListNode(l1_node.val)
-                    result = result.next
+                result.next = ListNode(l1_node.val)
+                result = result.next
                 l1_node = l1_node.next
             else:
-                if not result:
-                    result = ListNode(l2_node.val)
-                    f = result
-                else:
-                    result.next = ListNode(l2_node.val)
-                    result = result.next
+                result.next = ListNode(l2_node.val)
+                result = result.next
                 l2_node = l2_node.next
         while l1_node:
             t = ListNode(l1_node.val)
@@ -59,23 +51,24 @@ class Solution:
             result.next = t
             result = t
             l2_node = l2_node.next
-        return 
+        f = f.next
+        return f
 
-s = Solution()
-l1 = ListNode(1)
-t = l1
-for v in range(10, 20, 3):
-    t.next = ListNode(v)
-    t = t.next 
-l2 = ListNode(15)
-t = l2
-for v in range(0, 20, 4):
-    t.next = ListNode(v)
-    t = t.next
-p = s.mergeTwoLists(l1, l2)
-while p:
-    print(p.val)
-    p = p.next
+# s = Solution()
+# l1 = ListNode(1)
+# t = l1
+# for v in (2, 4):
+#     t.next = ListNode(v)
+#     t = t.next 
+# l2 = ListNode(1)
+# t = l2
+# for v in range(3, 5):
+#     t.next = ListNode(v)
+#     t = t.next
+# p = s.mergeTwoLists(l1, l2)
+# while p:
+#     print(p.val)
+#     p = p.next
 
 
 
